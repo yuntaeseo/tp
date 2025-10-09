223466231df0d96855b82db67d2eb51791294521
gordon <gordontaylol@gmail.com>
2025-10-10 01:23:53 +0800

    Fix import order

--- src/test/java/seedu/address/logic/commands/AddCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AddCommandTest.java
@@ -1,14 +1,17 @@
 package seedu.address.logic.commands;
 
-import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.Arrays;
 import static java.util.Objects.requireNonNull;
-import java.util.function.Predicate;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.address.testutil.Assert.assertThrows;
+import static seedu.address.testutil.TypicalPersons.ALICE;
+
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.function.Predicate;
+
 import org.junit.jupiter.api.Test;
 
 import javafx.collections.ObservableList;
@@ -21,9 +24,7 @@ import seedu.address.model.ReadOnlyAddressBook;
 import seedu.address.model.ReadOnlyUserPrefs;
 import seedu.address.model.person.Person;
 import seedu.address.model.tag.Tag;
-import static seedu.address.testutil.Assert.assertThrows;
 import seedu.address.testutil.PersonBuilder;
-import static seedu.address.testutil.TypicalPersons.ALICE;
 
 public class AddCommandTest {
 
