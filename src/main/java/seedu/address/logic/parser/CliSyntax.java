53e1961d91cc308a8627fe8724c3c2669f7d464a
XKMai <xkmai014@gmail.com>
2025-10-09 21:24:55 +0800

    Add Syntax for Tag CLI

--- src/main/java/seedu/address/logic/parser/CliSyntax.java
+++ src/main/java/seedu/address/logic/parser/CliSyntax.java
@@ -11,5 +11,8 @@ public class CliSyntax {
     public static final Prefix PREFIX_EMAIL = new Prefix("e/");
     public static final Prefix PREFIX_ADDRESS = new Prefix("a/");
     public static final Prefix PREFIX_TAG = new Prefix("t/");
+    // Tag prefixes
+    public static final Prefix PREFIX_DESC = new Prefix("d/");
+    public static final Prefix PREFIX_COLOR = new Prefix("c/");
 
 }
