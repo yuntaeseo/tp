37063b9b24abf3542adaab9f580f4fe87b76938b
XKMai <xkmai014@gmail.com>
2025-10-09 21:35:15 +0800

    Add AddTagCommand to implement tag addition functionality

---
+++ src/main/java/seedu/address/logic/commands/AddTagCommand.java
@@ -0,0 +1,36 @@
+package seedu.address.logic.commands;
+
+import static java.util.Objects.requireNonNull;
+
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.Model;
+import seedu.address.model.tag.Tag;
+
+public class AddTagCommand extends Command {
+    public static final String COMMAND_WORD = "addtag";
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Adds a tag to the address book. "
+            + "Parameters: "
+            + "n/NAME d/DESCRIPTION c/COLOR\n"
+            + "Example: " + COMMAND_WORD + " n/Friends d/Schoolmates c/RRGGBB";
+
+    public static final String MESSAGE_SUCCESS = "New tag added: %1$s";
+
+    private final Tag toAdd;
+
+    public AddTagCommand(Tag tag) {
+        requireNonNull(tag);
+        toAdd = tag;
+    }
+
+    @Override
+    public CommandResult execute(Model model) throws CommandException {
+        requireNonNull(model);
+
+        if (model.hasTag(toAdd)) {
+            throw new CommandException("This tag already exists in the address book");
+        }
+
+        model.addTag(toAdd);
+        return new CommandResult(String.format(MESSAGE_SUCCESS, toAdd));
+    }
+}
