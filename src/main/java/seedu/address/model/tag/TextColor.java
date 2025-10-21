ca55ae8702fc412ae2af09e3d28535031031a171
gordon <gordontaylol@gmail.com>
2025-10-21 17:20:32 +0800

    Add input validation for TextColor constructor

--- src/main/java/seedu/address/model/tag/TextColor.java
+++ src/main/java/seedu/address/model/tag/TextColor.java
@@ -1,14 +1,26 @@
 package seedu.address.model.tag;
 
+import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.util.AppUtil.checkArgument;
+
 import seedu.address.commons.util.ColorUtil;
 
 /**
  * Represents a text's color in RGB format.
  */
 public class TextColor {
+    public static final String MESSAGE_CONSTRAINTS = "Text colors should be in HEX format (without the '#'): RRGGBB";
+
+    /**
+     * Regex for enforcing the HEX format (without the '#' at the beginning).
+     */
+    public static final String VALIDATION_REGEX = "^(?:[0-9a-fA-F]{3}){1,2}$";
+
     public final String value;
 
     public TextColor(String value) {
+        requireNonNull(value);
+        checkArgument(isValidTextColor(value), MESSAGE_CONSTRAINTS);
         this.value = value;
     }
 
@@ -18,6 +30,13 @@ public class TextColor {
      */
     public static TextColor fromTagColor(TagColor backgroundColor) {
         return new TextColor(ColorUtil.getTextColor(backgroundColor.value));
+    }
+
+    /**
+     * Returns true if a given string is a valid text color.
+     */
+    public static boolean isValidTextColor(String test) {
+        return test.matches(VALIDATION_REGEX);
     }
 
     @Override
