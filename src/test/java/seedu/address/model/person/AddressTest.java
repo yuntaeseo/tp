3e9cc384cdb5d913d9cca0ad9caa8ca63dfda25a
duyLeu <anhduyleu15@gmail.com>
2025-10-24 14:54:02 +0800

    Minor changes to tests

--- src/test/java/seedu/address/model/person/AddressTest.java
+++ src/test/java/seedu/address/model/person/AddressTest.java
@@ -1,5 +1,6 @@
 package seedu.address.model.person;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.testutil.Assert.assertThrows;
@@ -40,6 +41,7 @@ public class AddressTest {
 
         // same values -> returns true
         assertTrue(address.equals(new Address("Valid Address")));
+        assertEquals(address.hashCode(), (new Address("Valid Address").hashCode()));
 
         // same object -> returns true
         assertTrue(address.equals(address));
