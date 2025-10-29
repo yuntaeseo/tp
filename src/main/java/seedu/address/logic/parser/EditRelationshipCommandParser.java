b3348a0b85146476c0074095dc40e1b2c1db97ce
XKMai <xkmai014@gmail.com>
2025-10-29 20:35:43 +0800

    Update EditRelationship Command Parser to edit only description

--- src/main/java/seedu/address/logic/parser/EditRelationshipCommandParser.java
+++ src/main/java/seedu/address/logic/parser/EditRelationshipCommandParser.java
@@ -6,13 +6,13 @@ import static seedu.address.logic.parser.CliSyntax.PREFIX_DESC;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_PART_1;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_PART_2;
 
-import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.EditRelationshipCommand;
-import seedu.address.logic.commands.EditRelationshipCommand.EditRelationshipDescriptor;
 import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.description.Description;
+import seedu.address.model.id.Id;
 
 /**
- * Parses input arguments and creates a new EditRelationshipCommand object.
+ * Parses input arguments and creates a new {@link EditRelationshipCommand} object.
  */
 public class EditRelationshipCommandParser implements Parser<EditRelationshipCommand> {
 
@@ -22,34 +22,17 @@ public class EditRelationshipCommandParser implements Parser<EditRelationshipCom
         ArgumentMultimap argMultimap =
                 ArgumentTokenizer.tokenize(args, PREFIX_PART_1, PREFIX_PART_2, PREFIX_DESC);
 
-        Index index;
-
-        try {
-            index = ParserUtil.parseIndex(argMultimap.getPreamble());
-        } catch (ParseException pe) {
-            throw new ParseException(
-                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, EditRelationshipCommand.MESSAGE_USAGE), pe);
+        if (!argMultimap.getValue(PREFIX_PART_1).isPresent()
+                || !argMultimap.getValue(PREFIX_PART_2).isPresent()
+                || !argMultimap.getValue(PREFIX_DESC).isPresent()
+                || !argMultimap.getPreamble().isEmpty()) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, EditRelationshipCommand.MESSAGE_USAGE));
         }
 
-        argMultimap.verifyNoDuplicatePrefixesFor(PREFIX_PART_1, PREFIX_PART_2, PREFIX_DESC);
+        Id part1 = ParserUtil.parseId(argMultimap.getValue(PREFIX_PART_1).get());
+        Id part2 = ParserUtil.parseId(argMultimap.getValue(PREFIX_PART_2).get());
+        Description desc = ParserUtil.parseDescription(argMultimap.getValue(PREFIX_DESC).get());
 
-        EditRelationshipDescriptor editRelationshipDescriptor = new EditRelationshipDescriptor();
-
-        if (argMultimap.getValue(PREFIX_PART_1).isPresent()) {
-            editRelationshipDescriptor.setPart1(ParserUtil.parseId(argMultimap.getValue(PREFIX_PART_1).get()));
-        }
-        if (argMultimap.getValue(PREFIX_PART_2).isPresent()) {
-            editRelationshipDescriptor.setPart2(ParserUtil.parseId(argMultimap.getValue(PREFIX_PART_2).get()));
-        }
-        if (argMultimap.getValue(PREFIX_DESC).isPresent()) {
-            editRelationshipDescriptor.setDescription(
-                    ParserUtil.parseDescription(argMultimap.getValue(PREFIX_DESC).get()));
-        }
-
-        if (!editRelationshipDescriptor.isAnyFieldEdited()) {
-            throw new ParseException(EditRelationshipCommand.MESSAGE_NOT_EDITED);
-        }
-
-        return new EditRelationshipCommand(index, editRelationshipDescriptor);
+        return new EditRelationshipCommand(part1, part2, desc);
     }
 }
