c41c01a5cbdd95ee1b0e826fe0f2ca207d6d2891
gordon <gordontaylol@gmail.com>
2025-10-30 19:47:19 +0800

    Fix Description error message

--- src/main/java/seedu/address/model/description/Description.java
+++ src/main/java/seedu/address/model/description/Description.java
@@ -7,8 +7,7 @@ import static seedu.address.commons.util.AppUtil.checkArgument;
  * Represents a description (for anything).
  */
 public class Description {
-    public static final String MESSAGE_CONSTRAINTS =
-            "Description should not be empty and should not have leading or trailing whitespaces";
+    public static final String MESSAGE_CONSTRAINTS = "Description should not be empty";
 
     public final String value;
 
@@ -25,7 +24,7 @@ public class Description {
 
     /**
      * Returns true if a given string is a valid description.
-     * The description should not be blank, and should not contain leading or trailing whitespaces.
+     * The description should not be blank.
      */
     public static boolean isValidDescription(String test) {
         return test.trim().equals(test) && !test.trim().isEmpty();
